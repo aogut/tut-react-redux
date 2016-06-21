5090734410f13c3b9c28583de22705c9c41318c0
aogut <andrew.ao@gmail.com>
2016-06-21 14:17:53 -0700

    Define routes

--- client/reduxstagram.js
+++ client/reduxstagram.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+// react router dependencies
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+
 // css
 import css from './styles/style.styl';
-import Main from './components/Main';
 
-render(<Main><p>hello</p></Main>, document.getElementById('root'));
+// components
+import Main from './components/Main';
+import PhotoGrid from './components/PhotoGrid';
+import Single from './components/Single';
+
+const route = (
+  <Router history={browserHistory}>
+    <Route path="/" component={Main}>
+      // Children
+      <IndexRoute component={PhotoGrid}></IndexRoute>
+      <Route path="/view/:postId" component={Single}></Route>
+    </Route>
+  </Router>
+)
+
+
+render(route, document.getElementById('root'));
