550b30c315a42dd2ee41f939ad9e01a7c4829c72
aogut <andrew.ao@gmail.com>
2016-06-22 10:33:55 -0700

    Display data in UI elements

--- client/components/PhotoGrid.js
+++ client/components/PhotoGrid.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Photo from './Photo';
 
 const PhotoGrid = React.createClass({
   render() {
     return (
-      <div class="photo-grid">
-        i am the photo grid
+      <div className="photo-grid">
+        {
+          this.props.posts.map(
+            (post, i) => <Photo {...this.props} key={i} i={i} post={post} />
+          )
+        }
       </div>
     )
   }
