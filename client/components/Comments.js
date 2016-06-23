d0c715a283262b267b24e7e5849bf22630857352
Andrew Ao <andrew.ao@gmail.com>
2016-06-22 21:18:23 -0700

    Create a second view

---
+++ client/components/Comments.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Comments = React.createClass({
+  render() {
+    return (
+      <div className="comments">
+        this is a comments
+      </div>
+    )
+  }
+});
+
+export default Comments;
