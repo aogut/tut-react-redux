831423558b71ffe23a13b124bb38e8a01c993db1
Andrew Ao <andrew.ao@gmail.com>
2016-06-23 20:05:36 -0700

    Create UI handler to dispatch the comment action

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -13,11 +13,20 @@ const Comments = React.createClass({
       </div>
     );
   },
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log(e);
+    console.log(this.refs);
+    const { postId } = this.props.params;
+    const author = this.refs.author.value;
+    const comment = this.refs.comment.value;
+    this.props.addComment(postId, author, comment);
+  },
   render() {
     return (
       <div className="comments">
         {this.props.postComments.map(this.renderComment)}
-        <form ref="commnetForm" className="comment-form">
+        <form ref="commnetForm" className="comment-form" onSubmit={this.handleSubmit}>
           <input type="text" ref="author" placeholder="author" />
           <input type="text" ref="comment" placeholder="comment" />
           <input type="submit" hidden />
